eba26fc34087d8a04d60abada5bc939e9c25bca3
buivanhuan1802 <chinhlaanh20019718@gmail.com>
2020-11-02 21:22:42 +0700

    add firebase dependencies

--- app/src/main/java/com/project/final_project_fall_2020/view/supplier/LoginActivity.java
+++ app/src/main/java/com/project/final_project_fall_2020/view/supplier/LoginActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.project.final_project_fall_2020.R;
 
 public class LoginActivity extends AppCompatActivity implements  View.OnClickListener {
@@ -13,6 +15,7 @@ public class LoginActivity extends AppCompatActivity implements  View.OnClickLis
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
 
     }
 
